ec6e0f328e847aa848e178ff7196d159a18c8a7b
david <david_2709@hotmail.com>
2019-09-25 00:02:34 -0500

    Select true positive

--- delf/selectImages.py
+++ delf/selectImages.py
@@ -3,6 +3,18 @@ from collections import Counter
 import pickle
 import operator
 from config import Config
+from delf import feature_io
+import hnswlib
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import boto3
+
+
+# Create an S3 client
+s3 = boto3.client('s3')
 
 model = smh.listdb_load(Config.MODEL_FILE)
 ifs = smh.listdb_load(Config.INVERT_INDEX_FILE)
@@ -17,7 +29,7 @@ with open(groundTruthFile, 'rb') as handle:
 
 
 # Create array with all images associated with a objectDiscovered
-objectDiscovered = model.ldb[32]
+objectDiscovered = model.ldb[74]
 imageCount={}
 for visualWord in objectDiscovered:
 	visualWordName = visualWord.item
@@ -27,6 +39,47 @@ for visualWord in objectDiscovered:
                 imageCount[image.item] += 1
 
 sort = sorted(imageCount.items(), key=lambda x: x[1], reverse=True)
-fileName = idToFileName[sort[0][0]]
+'''
+for i in range(len(sort)):
+	if idToFileName[sort[i][0]] not in groundTruthImages['10900']:
+		print(i)
+		break
+
+567
+'''	
+idLand = 0
+print(sort[idLand])
+fileName = idToFileName[sort[idLand][0]]
+print(fileName in groundTruthImages['13526'])
 print(fileName)
-print(fileName in groundTruthImages['10900'])
+
+inputpath  = '../../google_landmark_attention_selected/'
+image_path = '../../google_landmark_selected/'
+locations, _, descriptors, _, _ = feature_io.ReadFromFile(inputpath+fileName+'.delf')
+# Reiniting, loading the index
+p = hnswlib.Index(space='l2', dim=40)
+p.load_index("150000/hsm_150000clu_30iter_google_20land_2000balan.bin", max_elements = 150000)
+labels, _ = p.knn_query(descriptors, k=1)
+
+indexArray = []
+for i in range(descriptors.shape[0]):
+	for visualWord in objectDiscovered:
+        	visualWordName = visualWord.item
+		if labels[i] == visualWordName:
+			indexArray.append(i)
+                        break
+
+print(len(indexArray))
+fig = plt.figure(frameon=False)
+img_1 = mpimg.imread(image_path+fileName+'.jpg')
+
+ax = plt.Axes(fig, [0., 0., 1., 1.])
+ax.set_axis_off()
+fig.add_axes(ax)
+
+im = ax.imshow(img_1, cmap='gray',aspect='normal')
+ax.scatter(locations[indexArray, 1],locations[indexArray, 0], color='red',alpha=0.5)
+
+plt.savefig(fileName+'.jpg')
+# files automatically and upload parts in parallel.
+s3.upload_file(fileName+'.jpg', 'davtempbuck', fileName+'.jpg')
